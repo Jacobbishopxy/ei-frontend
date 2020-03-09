2cf4ff7637d470e76eca5b818d21c531e9becfd3
Jacob Xie <jacobbishopxy@gmail.com>
2020-03-09 16:47:47 +0800

    MNT: update demo/XlxsDataGrid -- add general convertData method

--- src/pages/demo/XlxsDataGrid/index.jsx
+++ src/pages/demo/XlxsDataGrid/index.jsx
@@ -4,45 +4,73 @@
 
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import React, { useState, useEffect } from 'react';
-import { Button, Input } from 'antd';
+import { Row, Col, Button, Input, Table, message } from 'antd';
 import Papa from 'papaparse';
+import _ from "lodash";
 
 import styles from './index.less';
 
+const antdTableColumnsGenerator = titles =>
+  titles.map((item, index) => ({title: item, dataIndex: item, key: index}));
 
+const papaConfig = {
+  skipEmptyLines: true,
+  header: true
+};
+
+const convertData = (titles, rawData) => {
+  const cd = Papa.parse(rawData, papaConfig).data;
+  if (cd.length === 0) {
+    message.error('转换失败');
+    return [];
+  }
+
+  const cdCheck = cd.filter(i => _.intersection(i, titles).length !== 0);
+  if (cdCheck.length === 0) {
+    message.error('转换失败');
+    return [];
+  }
+
+  message.success('转换成功');
+  return cd.map((item, index) => ({...item, key: index}))
+};
+
+
+const titles = ['时间', '全国', '华北', '东北', '华东', '中南', '西南', '西北'];
+
+const columns = antdTableColumnsGenerator(titles);
 
 export default () => {
-  const [clipboard, setClipboard] = useState("");
+  const [rawData, setRawData] = useState('');
+  const [convertedData, setConvertedData] = useState([]);
 
-  const readFromClipboard = ({target: {value}}) => {
-    try {
-      const convertedData = Papa.parse(value, {skipEmptyLines: true}).data;
-      console.log(convertedData);
-      setClipboard(convertedData);
-    } catch (e) {
-      console.log('parsing error: ');
-      console.log(e);
-    }
-  };
+  const readFromTextArea = ({target: {value}}) => setRawData(value);
+  const cvtData = () => setConvertedData(convertData(titles, rawData));
 
   return (
     <PageHeaderWrapper>
+      <Row>
+        <Col offset={6} span={12}>
+          <Input.TextArea
+            rows={4}
+            allowClear
+            onBlur={readFromTextArea}
+          />
+          <br/>
 
-      <Input.TextArea
-        rows={4}
-        allowClear
-        onBlur={readFromClipboard}
-      />
-      <br/>
+          <Button
+            onClick={cvtData}
+          >
+            转换数据
+          </Button>
 
-      <Button
-        onClick={() => alert(`your clipboard contains: ${clipboard}`)}
-      >
-        读取数据
-      </Button>
-
-
-
+          <Table
+            columns={columns}
+            dataSource={convertedData}
+            size='small'
+          />
+        </Col>
+      </Row>
     </PageHeaderWrapper>
   )
 };
