5ba404a3718287987b977d2e93305f80bf3bb197
Jacob Xie <jacobbishopxy@gmail.com>
2020-08-07 14:22:31 +0800

    RFT: update demo/CallChildFuncFromParent -- add childGenerator & childSwitcher

--- src/pages/demo/CallChildFuncFromParent/index.tsx
+++ src/pages/demo/CallChildFuncFromParent/index.tsx
@@ -13,19 +13,51 @@ interface RefObject {
   sayHi: () => void
 }
 
-export const Child = forwardRef((props: { name: string }, ref: React.Ref<RefObject>) => {
+interface ChildGeneratorProps {
+  greeting: string
+  forwardedRef: React.Ref<RefObject>
+}
 
-  const {name} = props;
-
-  const sayHi = () => console.log(`Saying hello from: ${name}`)
-
-  useImperativeHandle(ref, () => ({sayHi}));
-
-  return <div>{name}</div>
-});
+interface ChildProps {
+  name: string
+}
 
 
-export const Parent = () => {
+const childGenerator = (cgProps: ChildGeneratorProps) => {
+
+  return forwardRef((frProps: ChildProps, ref: React.Ref<RefObject>) => {
+
+    const {name} = frProps;
+
+    const sayHi = () => console.log(`Saying hello from: ${name}`)
+
+    useImperativeHandle(ref, () => ({sayHi}));
+
+    return <div>{cgProps.greeting} {name}</div>
+  });
+};
+
+
+const childSwitcher = (name: string, switcher: string) => (props: ChildGeneratorProps) => {
+
+  const Child1 =
+    childGenerator({greeting: `Child #1: ${props.greeting}`, forwardedRef: props.forwardedRef});
+  const Child2 =
+    childGenerator({greeting: `Child #2: ${props.greeting}`, forwardedRef: props.forwardedRef});
+
+
+  switch (switcher) {
+    case '1':
+      return <Child1 name={name} ref={props.forwardedRef}/>;
+    case '2':
+      return <Child2 name={name} ref={props.forwardedRef}/>;
+    default:
+      return <Child1 name={name} ref={props.forwardedRef}/>;
+  }
+}
+
+
+const Parent = () => {
 
   const childRef = useRef<RefObject>(null);
 
@@ -33,9 +65,13 @@ export const Parent = () => {
     if (childRef.current) childRef.current.sayHi();
   }
 
+  const s = childSwitcher('Jacob', '2')
+
   return (
     <div onClick={onClick}>
-      <Child name="Jacob" ref={childRef}/>
+      {
+        s({greeting: 'Hi', forwardedRef: childRef})
+      }
     </div>
   );
 };
